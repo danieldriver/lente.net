765df2c78cb2a7e72f6875d3276a047a3ed2a2ca
Daniel Driver <danieldriver@me.com>
2013-03-17 08:38:14 -0400

    specify function(i)
    probably necessary - works either way

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // position footnotes in margin 
 $(document).ready(function() {
-	$('.footnoteRef').each(function() {
+	$('.footnoteRef').each(function(i) {
 		var fnid = $(this).attr('href');
 		var pos = $(this).position();
 		$('.footnotes').css("top", "0px");
